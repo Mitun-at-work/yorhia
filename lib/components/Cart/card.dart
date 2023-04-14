744449311afdca184dda43ab20a8beac1a183a39
Johnvimalasir001 <johnvimalasir@gmail.com>
2023-04-14 16:05:42 +0530

    cart page updated

--- lib/components/Cart/card.dart
+++ lib/components/Cart/card.dart
@@ -2,129 +2,118 @@ import 'package:flutter/material.dart';
 import 'package:yoriha/theme/colors.dart';
 
 class CardPage extends StatelessWidget {
-  const CardPage({Key? key}) : super(key: key);
+  const CardPage({Key? key, required this.imageurl}) : super(key: key);
+  final String imageurl;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 150,
-      width: 350,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      margin: const EdgeInsets.only(
+        top: 25,
+        right: 10,
+        left: 10,
+      ),
       decoration: const BoxDecoration(
         color: appBarColor,
         borderRadius: BorderRadius.all(
           Radius.circular(
-            35,
+            15,
           ),
         ),
       ),
-      child: Column(
+      child: Row(
+        // mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Row(
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(
-                  top: 40,
-                  left: 30,
-                  right: 15,
-                ),
-                child: SizedBox(
-                  height: 70,
-                  width: 70,
-                  child: Image(
-                    image: NetworkImage(
-                      "https://i.pinimg.com/originals/82/71/fe/8271feb1204633e8d46d5de57cff4a2c.png",
-                    ),
-                    fit: BoxFit.cover,
+          Container(
+            height: 120,
+            width: 140,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(
+                Radius.circular(25),
+              ),
+              color: Colors.black,
+              image: DecorationImage(
+                image: NetworkImage(imageurl),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(
+              top: 10,
+              bottom: 0,
+              left: 5,
+              right: 100,
+            ),
+            margin: const EdgeInsets.only(left: 5),
+            child: Column(
+              children: [
+                // margin: const EdgeInsets.only(bottom: 10),
+                const Text(
+                  "Apple",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
                   ),
+                ),
+
+                const Text(
+                  "Organic",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: lightText,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+
+                Container(
+                  alignment: Alignment.center,
+                  height: 40,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    color: highlightColor,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: const Text(
+                    "12Kg",
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.cancel_rounded,
+                  color: Colors.white,
                 ),
               ),
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      const Text(
-                        "Orange",
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontFamily: 'Poppins',
-                          color: Colors.white,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 100,
-                      ),
-                      InkWell(
-                        child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.cancel_rounded,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ],
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                children: const [
+                  Icon(
+                    Icons.currency_rupee_sharp,
+                    size: 18,
+                    color: lightText,
                   ),
-                  const Padding(
-                    padding: EdgeInsets.only(
-                      right: 170,
+                  Text(
+                    "100",
+                    style: TextStyle(
+                      color: lightText,
+                      fontSize: 18,
                     ),
-                    child: Text(
-                      "Organic",
-                      style: TextStyle(
-                        color: lightText,
-                        fontFamily: 'Poppins',
-                        fontSize: 15,
-                      ),
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 20,
-                          right: 90,
-                        ),
-                        child: Container(
-                          alignment: Alignment.center,
-                          height: 40,
-                          width: 60,
-                          decoration: BoxDecoration(
-                            color: highlightColor,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: const Text(
-                            "12Kg",
-                            style: TextStyle(
-                              color: Colors.black87,
-                              fontFamily: 'Poppins',
-                            ),
-                          ),
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(
-                          top: 40,
-                          // right: 5,
-                        ),
-                        child: SizedBox(
-                          // alignment: Alignment.center,
-                          height: 40,
-                          width: 80,
-                          // decoration: BoxDecoration(
-                          //   color: appBarColor,
-                          //   borderRadius: BorderRadius.circular(8),
-                          // ),
-                          child: Text(
-                            "\$100.0",
-                            style: TextStyle(
-                              color: lightText,
-                              fontSize: 20,
-                              fontFamily: 'Poppins',
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
                   ),
                 ],
               ),
--- lib/components/Cart/cart_page.dart
+++ lib/components/Cart/cart_page.dart
@@ -5,6 +5,14 @@ import 'package:yoriha/theme/colors.dart';
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
+  static List<String> imageLinks = [
+    "https://www.shutterstock.com/image-photo/red-apple-isolated-on-white-600w-1727544364.jpg",
+    "https://www.bookmymango.in/wp-content/uploads/2021/04/gir-kesar.png",
+    "https://www.shutterstock.com/image-photo/ripe-orange-isolated-on-white-260nw-606022676.jpg",
+    "https://media.istockphoto.com/id/1400057530/photo/bananas-isolated.jpg?b=1&s=170667a&w=0&k=20&c=uiSdjIQkTr7S4gEdW_oB_5zfFYhpfe0LP-CryQl49w4=",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrn4mlV6Yb0_z-YbsE3yyFR47qG8kwcP35Q_Mj19Vl&s",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,10 +28,12 @@ class CartPage extends StatelessWidget {
                 ),
                 child: InkWell(
                   child: IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
                     icon: const Icon(
                       Icons.arrow_back_ios,
-                      size: 25,
+                      size: 20,
                       color: Colors.white,
                     ),
                   ),
@@ -42,7 +52,7 @@ class CartPage extends StatelessWidget {
                       child: Text(
                         "My Cart",
                         style: TextStyle(
-                          fontSize: 30,
+                          fontSize: 25,
                           color: Colors.white,
                           fontFamily: 'Poppins',
                         ),
@@ -53,46 +63,47 @@ class CartPage extends StatelessWidget {
               ),
             ],
           ),
-          Flexible(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 10,
-              ),
-              child: GridView.builder(
-                itemCount: 5,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 1,
-                  childAspectRatio: 2.3,
-                  mainAxisSpacing: 20,
+          Expanded(
+            child: Column(
+              children: [
+                Flexible(
+                  child: ListView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: imageLinks.length,
+                    itemBuilder: (context, index) {
+                      return CardPage(imageurl: imageLinks[index]);
+                    },
+                  ),
                 ),
-                itemBuilder: (context, index) => const CardPage(),
-              ),
-            ),
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.only(
-                bottom: 10,
-              ),
-              child: SizedBox(
-                height: 50,
-                width: 350,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: highlightColor,
-                      shape: const StadiumBorder()),
-                  child: const Text(
-                    "\$300 Check Out",
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontFamily: 'Poppins',
-                      color: Colors.black,
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      top: 10,
+                      bottom: 10,
+                    ),
+                    child: SizedBox(
+                      height: 50,
+                      width: 350,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: highlightColor,
+                          shape: const StadiumBorder(),
+                        ),
+                        child: const Text(
+                          "\$300 Check Out",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontFamily: 'Poppins',
+                            color: Colors.black87,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
+              ],
             ),
           ),
         ],
