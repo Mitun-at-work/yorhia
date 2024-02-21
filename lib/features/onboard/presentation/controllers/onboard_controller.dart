5be37a45baef704038e4aa2d2617b0937c8a3733
Guru <mguruprasanth2004@gmail.com>
2024-02-21 18:24:47 +0530

    integrated branches

--- lib/features/onboard/presentation/controllers/onboard_controller.dart
+++ lib/features/onboard/presentation/controllers/onboard_controller.dart
@@ -1,7 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../../db/local/hive_auth_manager.dart';
 import '../../../../db/remote/firebase_manage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
 
 class OnboardController extends GetxController {
   late AuthenticationManager authManager;
@@ -23,6 +27,23 @@ class OnboardController extends GetxController {
       await Future.delayed(const Duration(seconds: 5));
       // Get.offAndToNamed(AppRouteNames.homeRoute);
     } else {}
+  }
+
+  Future<void> isUserAuthenticate() async {
+    final bool isAuthenticated = await authManager.isUserAuthenticated();
+    if (!isAuthenticated) {
+      GSignin(BuildContext context) async {
+        GoogleSignInAccount? googleuser = await GoogleSignIn().signIn();
+        GoogleSignInAuthentication? googleAuth =
+            await googleuser?.authentication;
+        AuthCredential credential = GoogleAuthProvider.credential(
+            accessToken: googleAuth?.accessToken, idToken: googleAuth?.idToken);
+        UserCredential userCredential =
+            await FirebaseAuth.instance.signInWithCredential(credential);
+        //Navigator.push(
+        //context, MaterialPageRoute(builder: (context) => HomeScreen()));
+      }
+    }
   }
 
   Future<bool> isUserAuthenticated() async {
