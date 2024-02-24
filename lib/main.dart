9d34863c9b1b1ff360c89ba6d829714b45611e45
Mitun Suresh <mitun.focused@gmail.com>
2024-02-24 15:04:48 +0530

    fefee

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ Future<void> main() async {
       child: GetMaterialApp(
         debugShowCheckedModeBanner: false,
         theme: getTheme,
-        color: Colors.orange,
+        color: Colors.voilet,
         routes: appRoutes.getRoutes(),
         initialRoute: AppRouteNames.onboardRoute,
       ),
